0ace8c580edd0ec9524386430b135d3b3c99705b
hgn <git@hgn.me>
2024-12-09 22:45:47 +1100

    lazy stop logging of infinite DHCPs

--- packet_handlers.py
+++ packet_handlers.py
@@ -51,9 +51,10 @@ class PacketHandler:
             return xml.find(tag)
         return self.xml_data.find(tag)
     
-    def set_packet_type(self, packet_type: str):
+    def set_packet_type(self, packet_type: str, should_log = True):
         self.packet_type = packet_type
-        self.logger.debug(f"({self.packet_id}) Packet Type is: {self.packet_type}")
+        if should_log:
+            self.logger.debug(f"({self.packet_id}) Packet Type is: {self.packet_type}")
 
     def process_sip_id_request(self):
         self.logger.info(f"({self.packet_id}) Processing SIP ID request")
@@ -74,9 +75,15 @@ class PacketHandler:
         unlocked_by = f"{elev_floor}{apt_number}"
         self.logger.info(f"({self.packet_id}) Elevator unlocked for Building {elev_building}, floor {elev_floor} by apt {unlocked_by}")
     
+    def decode_search_ack(self):
+        resp_id = self.get_xml_value_from_tag('id').text
+        resp_ip = self.get_xml_value_from_tag('ip').text
+        resp_mac = self.get_xml_value_from_tag('mac').text
+        self.logger.info(f"search response. id, ip, mac.\t{resp_id},{resp_ip},{resp_mac}")
     def handle_event_packet(self):
         event_activity = self.get_xml_value_from_tag('active').text
         event_type = self.get_xml_value_from_tag('type').text
+        self.logger.debug(f"({self.packet_id}) Contents:\n{self.packet}")
         if event_activity == "discover":
             # Someone wants a, or is reponding to a SIP address request.
             self.set_packet_type("xml->event->discover")
@@ -95,12 +102,17 @@ class PacketHandler:
                 if event_type == "req":
                     self.set_packet_type("xml->event->elevaction->req")
                     self.decode_elevator_request()
+        elif event_activity == "search":
+            if event_type == "ack":
+                self.set_packet_type("xml->event->search->ack")
+                self.decode_search_ack()
         else:
             self.logger.info(f"({self.packet_id}) Packet is a unknown event ({event_activity}:{event_type})")
 
     def handle_xml_packet(self):
         xml = self.xml_data
         if xml.tag == "dhcp":
+            return
             self.set_packet_type("xml->dhcp")
             # Handle DHCP Packet
         elif xml.tag == "event":
@@ -108,12 +120,12 @@ class PacketHandler:
             self.handle_event_packet()
             
     def handle_packet(self):
-        self.logger.info(f"({self.packet_id}) Handling packet from {self.packet.source_ip}:{self.packet.source_port}")
-        self.logger.debug(f"({self.packet_id}) Contents:\n{self.packet}")
+        # self.logger.info(f"({self.packet_id}) Handling packet from {self.packet.source_ip}:{self.packet.source_port}")
         if self.parse_xml(self.packet.data) is not None:
-            self.set_packet_type("xml")
+            self.set_packet_type("xml", False)
             self.handle_xml_packet()
         else:
             self.set_packet_type("non-xml")
             self.logger.warning(f"({self.packet_id}) Packet is non-XML")
+            self.logger.debug(f"({self.packet_id}) Contents:\n{self.packet}")
 

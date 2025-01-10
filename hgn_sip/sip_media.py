84717576332d80758bace105ba6dca2e01b14d22
hgn <git@hgn.me>
2025-01-10 22:58:14 +1100

    samplerate bump because the browser said 48k was right maybe

--- hgn_sip/sip_media.py
+++ hgn_sip/sip_media.py
@@ -6,7 +6,7 @@ port_logger = get_logger("dummy-audio-media-port")
 def get_audio_format() -> pj.MediaFormatAudio:
     af = pj.MediaFormatAudio()
     af.type = pj.PJMEDIA_TYPE_AUDIO
-    af.clockRate = 8000
+    af.clockRate = 48000
     af.channelCount = 1
     af.bitsPerSample = 16
     af.frameTimeUsec = 20000
